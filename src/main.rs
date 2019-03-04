cde331de0c6363cbb0cefe72db8473ad8a30430f
Christopher J. Stehno <chris.stehno@gmail.com>
2019-03-04 10:43:14 -0600

    Added delete support.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::time::Duration;
 use zookeeper::{WatchedEvent, Watcher, ZooKeeper};
 use zookeeper::Acl;
 use zookeeper::CreateMode;
+use zookeeper::Stat;
 
 struct ConnWatcher {}
 
@@ -13,12 +14,7 @@ impl Watcher for ConnWatcher {
     }
 }
 
-/*
-zk <conn-str> ls /
-zk <conn-str> set / <value>
-zk <conn-str> get /
-zk <conn-str> export / <file>
-*/
+// FIXME: what about user/pass and ssl (add_auth)?
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -33,7 +29,9 @@ fn main() {
         match (operation.as_str(), arg_count) {
             ("ls", 4) => list_node(conn_str, &args[3]),
             ("set", 5) => set_node_data(conn_str, &args[3], &args[4]),
+            ("rm", 4) => remove_node(conn_str, &args[3]),
             ("get", 4) => get_node_data(conn_str, &args[3]),
+            // FIXME: support export operation
             ("export", 5) => {
                 panic!("The export operation is not yet supported.")
             }
@@ -41,6 +39,8 @@ fn main() {
         }
     }
 }
+
+// FIXME: see if I can refactor away some duplication
 
 fn list_node(conn_str: &str, node_path: &str) {
     println!("{}", node_path);
@@ -57,18 +57,33 @@ fn list_node(conn_str: &str, node_path: &str) {
     }
 }
 
-fn get_node_data(conn_str: &str, node_path: &str) {
+fn remove_node(conn_str: &str, node_path: &str) {
     match ZooKeeper::connect(conn_str, Duration::new(30000, 0), ConnWatcher {}) {
         Ok(zk) => {
-            let (data, _stat) = zk.get_data(node_path, false).unwrap();
-            println!("{} = {:?}", node_path, String::from_utf8(data).unwrap());
+            match zk.delete(node_path, None) {
+                Ok(_) => println!("(Removed) {}", node_path),
+                Err(_) => panic!("Unable to delete node ({}) - does it have children?", node_path)
+            }
             zk.close().unwrap();
         }
         Err(e) => panic!("Unable to connect to {}: {:?}", conn_str, e)
     }
 }
 
-fn set_node_data(conn_str: &str, node_path: &str, node_value: &str){
+fn get_node_data(conn_str: &str, node_path: &str) {
+    match ZooKeeper::connect(conn_str, Duration::new(30000, 0), ConnWatcher {}) {
+        Ok(zk) => {
+            let (data, stat) = zk.get_data(node_path, false).unwrap();
+            println!("{} = {:?}", node_path, String::from_utf8(data).unwrap());
+            display_stat(&stat);
+
+            zk.close().unwrap();
+        }
+        Err(e) => panic!("Unable to connect to {}: {:?}", conn_str, e)
+    }
+}
+
+fn set_node_data(conn_str: &str, node_path: &str, node_value: &str) {
     match ZooKeeper::connect(conn_str, Duration::new(30000, 0), ConnWatcher {}) {
         Ok(zk) => {
             match zk.exists(node_path, false) {
@@ -76,27 +91,40 @@ fn set_node_data(conn_str: &str, node_path: &str, node_value: &str){
                     match opt {
                         Some(_) => {
                             let set_stat = zk.set_data(node_path, node_value.as_bytes().to_vec(), None).unwrap();
-                            println!("Updated: {}", node_path);
+                            println!("(Updated) {} = {}", node_path, node_value);
+                            display_stat(&set_stat);
                         }
                         None => {
                             match zk.create(node_path, node_value.as_bytes().to_vec(), Acl::open_unsafe().clone(), CreateMode::Persistent) {
                                 Ok(res) => {
-                                    println!("Created: {}", res);
+                                    println!("(Created) {} = {}", node_path, node_value);
                                 }
                                 Err(c_e) => panic!("Unable to create node {}: {}", node_path, c_e)
                             }
                         }
                     }
                 }
-                Err(err) => {
-                    panic!("Something bad has happened: {:?}", err);
-                }
+                Err(err) => panic!("Something bad has happened: {:?}", err)
             }
 
             zk.close().unwrap();
         }
         Err(e) => panic!("Unable to connect to {}: {:?}", conn_str, e)
     }
+}
+
+fn display_stat(stat: &Stat) {
+    println!("    czxid:           {}", stat.czxid);
+    println!("    mzxid:           {}", stat.mzxid);
+    println!("    ctime:           {}", stat.ctime);
+    println!("    mtime:           {}", stat.mtime);
+    println!("    version:         {}", stat.version);
+    println!("    cversion:        {}", stat.cversion);
+    println!("    aversion:        {}", stat.aversion);
+    println!("    ephemeral_owner: {}", stat.ephemeral_owner);
+    println!("    data_length:     {}", stat.data_length);
+    println!("    num_children:    {}", stat.num_children);
+    println!("    pzxid:           {}", stat.pzxid);
 }
 
 fn show_help() {
